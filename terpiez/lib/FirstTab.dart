7cd8b7c75bb140bab370124845d4d6bc02bd70d3
Aryan Shrestha <aryanshrestha3810@gmail.com>
2024-02-27 15:40:36 -0500

    Title is smaller in landscape

--- terpiez/lib/FirstTab.dart
+++ terpiez/lib/FirstTab.dart
@@ -5,27 +5,51 @@ class FirstTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Center(
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Statistics',
-              style: TextStyle(fontSize: 48),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text('Terpiez Found: 12', style: TextStyle(fontSize: 20)),
-                  Text('Days Active: 3', style: TextStyle(fontSize: 20)),
-                ],
-              ),
-            )
-      
-          ],
-        ),
+    return  SafeArea(
+      child: OrientationBuilder(
+        builder: (context,orientation) {
+          if (orientation == Orientation.portrait){
+            return const Column(
+              children: <Widget>[
+                Text(
+                  'Statistics',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text('Terpiez Found: 12', style: TextStyle(fontSize: 20)),
+                      Text('Days Active: 3', style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                ),
+            ]
+            );
+          }
+          else {
+            return const Column(
+                children: <Widget>[
+                  Text(
+                    'Statistics',
+                    style: TextStyle(fontSize: 36),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text('Terpiez Found: 12', style: TextStyle(fontSize: 14)),
+                        Text('Days Active: 3', style: TextStyle(fontSize: 14)),
+                      ],
+                    ),
+                  ),
+                ]
+            );
+          }
+
+        }
       ),
     );
   }
